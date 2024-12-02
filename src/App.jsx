8e75e8219376061bea23f30026e11d467937a22a
shu <shangshu1024@gmail.com>
2024-12-02 08:55:38 -0500

    UI updated

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import { About } from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Testimonails from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div className="w-full overflow-hidden">
+      <ToastContainer />
+      <Header />
+      <About />
+      <Projects />
+      <Testimonails />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { toast } from "react-toastify";
+import { motion } from "framer-motion";
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "acfa3fd3-bd64-49f0-8f20-6c6926179585");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+      toast.error(data.message);
+    }
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      <form
+        className="max-w-2xl mx-auto text-gray-600 pt-8"
+        onSubmit={onSubmit}
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              name="Email"
+              placeholder="Your Email"
+              required
+            />
+          </div>
+        </div>
+        <div className="text-left my-6">
+          Message
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="message"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </motion.div>
+  );
+};
+
+export default Contact;
